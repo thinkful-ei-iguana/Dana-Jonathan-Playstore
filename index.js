b1849c1aa5d5d008448fde6811dd2948e1697d77
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2019-12-03 11:21:33 -0800

    finished filtering by genre and sorting queries

--- index.js
+++ index.js
@@ -12,35 +12,43 @@ app.listen(8080, () => {
 });
 
 app.get('/apps', (req, res) => {
-  const { sort, genres } = req.query;
+  let { sort, genres } = req.query;
   let results = gapps;
   if (genres) {
     if (
-      !['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(
-        genres
+      !['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'].includes(
+        genres.toLowerCase()
       )
     ) {
       return res.status(400).send(
         // eslint-disable-next-line quotes
         "genres value must either be 'Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'"
       );
+    } else {
+      genres = genres.toLowerCase();
+      genres = genres[0].toUpperCase() + genres.slice(1);
+      results = results.filter(app => app['Genres'] === genres);
     }
   }
-
   if (sort) {
     if (!['rating', 'app'].includes(sort.toLowerCase())) {
       return res.status(400).send('sort value must either be rating or app');
+    } else {
+      if (sort.toLowerCase() === 'app') {
+        sort = sort.toLowerCase();
+        sort = sort[0].toUpperCase() + sort.slice(1); //makes the first letter capital
+        results = results.sort((a, b) => {
+          return a[sort].toLowerCase() > b[sort].toLowerCase()
+            ? 1
+            : a[sort].toLowerCase() < b[sort].toLowerCase()
+            ? -1
+            : 0;
+        });
+      } else {
+        sort = parseFloat(sort);
+        results = results.sort((a, b) => b.Rating - a.Rating);
+      }
     }
-  }else {
-    sort = sort.toLowerCase();
-    if (sort === 'rating') {
-      sort = parseFloat(sort);
-    }
-   
-      results.sort((a, b) => {
-      return a[sort].toLowerCase() > b[sort].toLowerCase() ? 1 : a[sort].toLowerCase() < b[sort].toLowerCase() ? -1 : 0;
-   });
-
   }
 
   res.json(results);
